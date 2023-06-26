e812eabd7f4c518342cef9b6e1ff48be21947379
alihsn9776 <123621135+alihsn9776@users.noreply.github.com>
2023-06-27 01:39:56 +0330

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 # Reading Data
-x_test <- read.table("F://JHU/Cleaning/UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("F://JHU/Cleaning/UCI HAR Dataset/test/y_test.txt")
-x_train <- read.table("F://JHU/Cleaning/UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("F://JHU/Cleaning/UCI HAR Dataset/train/y_train.txt")
-act_labs <- read.table("F://JHU/Cleaning/UCI HAR Dataset/activity_labels.txt")
-feats <- read.table("F://JHU/Cleaning/UCI HAR Dataset/features.txt")
-subject_test <- read.table("F://JHU/Cleaning/UCI HAR Dataset/test/subject_test.txt")
-subject_train <- read.table("F://JHU/Cleaning/UCI HAR Dataset/train/subject_train.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+act_labs <- read.table("UCI HAR Dataset/activity_labels.txt")
+feats <- read.table("UCI HAR Dataset/features.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 
 #Merging X and Y
@@ -26,7 +26,7 @@ for (i in 1:ncol(x_df)) {
         if (!grepl(pattern = "mean|std", names(x_df))[i]) {
                 x_df <- x_df[,-i]
         }
-}
+}       #Unfortunately you need to run this loop function 6-7 times (I don't know how to debug it), each time it omits a number of columns until it reaches 79
 
 grepl(pattern = "mean|std", names(x_df))        #79 TRUEs
 
@@ -39,7 +39,7 @@ for (i in 1:nrow(y_df)) {
                 }
         }
 }
-
+summary(as.factor(y_df$act_cat))
 
 #Merging dataset
 tot_df <- cbind(subjects,x_df, y_df)
@@ -54,3 +54,5 @@ names(tot_df) <- tolower(gsub(pattern = "\\()", replacement = "", x = names(tot_
 
 summary_df <- aggregate(. ~ subj + act_cat, data = tot_df, FUN = mean, na.rm = TRUE)
 summary_df <- arrange(.data = summary_df, subj, act_cat)
+
+write.table(summary_df, file = "project.txt", quote = FALSE, row.names = FALSE)
